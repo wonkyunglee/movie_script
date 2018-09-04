00e89024998a06a58d07134af09ad1f7fd9cbd52
wonkyunglee <leewk921223@gmail.com>
2018-09-04 21:45:04 +0900

    add flags

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
+import argparse
+
 from tokenizer import Tokenizer
 from parser import Parser
 from processor import Processor
 
 
-FILENAME = 'guardians_of_the_galaxy'
+#FILENAME = 'guardians_of_the_galaxy'
 #FILENAME = 'doctor_strange'
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", help="filename to process.")
+    args = parser.parse_args()
+    FILENAME = args.filename
+    
     with open(os.path.join('original_scripts', FILENAME + '.srt'), 'r') as f:
         string = f.read()
 
@@ -16,7 +23,7 @@ def main():
     script = parser.take_script(tokens)
     processor = Processor()
     processor.write_sentences(script, os.path.join('processed_scripts', FILENAME+'.txt'))
-
-
+    
+    
 if __name__=='__main__':
     main()
